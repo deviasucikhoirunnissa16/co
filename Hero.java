a22f9439afead776bed69119c5e5c02d7d7c250a
Hardz27 <maulana27051998@gmail.com>
2018-11-02 18:54:37 +0700

    coba

---
+++ Hero.java
@@ -0,0 +1,10 @@
+public class Hero{
+	public Hero(){
+		System.out.println("I'll save you");
+	}
+	public static void main(String[]args){
+		Hero h1 = new Hero();
+		
+	}
+	
+}
---
+++ Spiderman.java
@@ -0,0 +1,12 @@
+public class Spiderman extends Hero{
+	public Spiderman(){
+		super();
+		System.out.println("I'm Spiderman");
+		
+
+	}
+	public static void main(String[]args){
+		Spiderman sp1 = new Spiderman();
+		
+	}
+}
